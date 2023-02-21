67eba8da85f864e55ef57597f3e2912a10f0358d
Charles Sharp <25charlessharp@usd266.com>
2023-02-21 11:35:11 -0600

    done with rectangle

--- Main.java
+++ Main.java
@@ -14,15 +14,21 @@ public class Main {
 
 
   public static double [][] copyRectangle(double [][] rectangle){
-    int length = 0;
-    int hight = 0;
-    double [][] sameArray = new double[hight][length]
+    int length = 4;
+    int hight = 3;
+    double [][] sameArray = new double[hight][length];
+    for(int r = 0; r < hight; r ++){
+      for(int c = 0; c < length; c ++){
+        sameArray[r][c] = rectangle[r][c];
+      }
+    }
+    return sameArray;
   }
 
 
   public static void main(String[] args) {
-    double[][] orig = new double[][]{{1,2,3},{4,5,6},{7,8,9}};
-    double[][] newArr = copySquare(orig);
+    double[][] orig = new double[][]{{1,2,3,3},{4,5,6,6},{7,8,9,9}};
+    double[][] newArr = copyRectangle(orig);
     System.out.println(Arrays.deepToString(newArr));
   }
 }
