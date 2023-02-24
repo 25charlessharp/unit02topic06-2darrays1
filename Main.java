43957180cff6804f4de51809c598ccc4e6afc521
Charles Sharp <25charlessharp@usd266.com>
2023-02-24 12:09:32 -0600

    done with weighted value

--- Main.java
+++ Main.java
@@ -56,6 +56,41 @@ public class Main {
     
   }
 
+  public static double [][] withWeightedAverage(double [][] grades, double[] weighted){
+    int newRows = grades.length +1;
+    int newColls = grades[0].length + 1;
+    double average[][] = new double [newRows][newColls];
+    double addColls = 0.0;
+    double multRows = 0.0;
+    for(int r = 0; r < newRows-1; r++){
+      for(int c = 0; c < newColls; c++){
+        if(newColls - 1 == c){
+          average[r][c] = multRows;
+          multRows = 0.0;
+        }
+        else{
+        average[r][c] = grades[r][c];
+        multRows = average[r][c] * weighted[c] + multRows;
+        }
+      }
+      
+    }
+
+    for(int c = 0; c < newColls; c++){
+      for(int r = 0; r < newRows -1; r++){
+        if(newRows - 2 == r){
+          average[newRows-1][c] = addColls/grades.length;
+          addColls = 0.0;
+        }
+        else{
+          addColls = average[r][c] + addColls;
+        }
+      }
+    }
+      return average;
+    }
+
+
 
 
   public static void main(String[] args) {
@@ -71,9 +106,11 @@ public class Main {
     {77, 70.5, 66.5},
     {89, 89.5, 81}
     };
+
+    double [] orig2 = new double [] {0.25,0.25,0.5};
     
-    double[][] newArr = (orig);
+    double[][] newArr = withWeightedAverage(orig, orig2);
     System.out.println(Arrays.deepToString(newArr));
-    printTranspose(orig);
+    //printTranspose(orig);
   }
 }
